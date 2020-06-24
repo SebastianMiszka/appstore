37434eea76f620abee41d4efc9720d3b52a33035
SebastianMiszka <seeboom123@gmail.com>
2020-06-24 21:24:07 +0200

    2. Dodanie czasu

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,6 +1,24 @@
 package com.company;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Game {
+
+    LocalDate time = LocalDate.of(2020, 1, 1);
+
+    DayOfWeek DayOfWeek = time.getDayOfWeek();
+
+    public void Today() {
+        System.out.println("Zaczynasz grę, jest: " + time + " " + time.getDayOfWeek());
+    }
+
+    public void TimeAdd(){
+        time = time.plusDays(2);
+        System.out.println("Dzisiaj jest: " + time + " " + time.getDayOfWeek());
+    }
+
+
 
 
 }
---
+++ src/com/company/Project.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Project {
+
+    String name;
+}
